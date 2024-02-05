fdf9b9d82202b0395a34514bf585c768328eabec
venusaur20 <144698164+venusaur20@users.noreply.github.com>
2024-02-05 21:14:04 +0200

    made macro to click on chat then detect which troop has been requested

--- CoC-auto-donate.py
+++ CoC-auto-donate.py
@@ -1,6 +1,29 @@
-import dxcam
-from PIL import Image
+import pyautogui, time, keyboard
 
-camera = dxcam.create()
-frame = camera.grab()
-Image.fromarray(frame).show()
+######################################################## Gets Mouse Pos
+#while True:
+#    currentMouseX, currentMouseY = pyautogui.position()
+#    print(currentMouseX, currentMouseY)
+#    
+#    if keyboard.is_pressed("q"):
+#        break
+ 
+#    time.sleep(1)
+
+time.sleep(2)
+pyautogui.click(18, 470)
+
+
+while True:
+     troop = pyautogui.locateOnScreen(r'C:\Users\Hasan\Documents\GitHub-Hasan\CoC-Auto-Donate\CoC Troops\Barbarian Request.PNG', confidence = 0.9, grayscale = False, region = (3, 108, 582, 945))
+ 
+     pyautogui.leftClick(600, 488)
+     pyautogui.leftClick(70, 815)
+     pyautogui.leftClick(621, 173)
+     
+#     if troop == "barbarian":
+     pyautogui.leftClick(334, 618)
+     
+    
+     if keyboard.is_pressed("q"):
+         break
